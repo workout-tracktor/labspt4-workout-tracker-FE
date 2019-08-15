863fcfec1cdb94a7fff35db8ff18173b4bcb6834
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-08-14 20:43:06 -0700

    react native pulling backend api

--- native-workout-tracker/workout/App.js
+++ native-workout-tracker/workout/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Landing from './components/Landing'
+import {NativeRouter, Switch, Route} from 'react-router-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NativeRouter>
+      <View style = {styles.container}>
+        <Switch>
+          <Route exact path = "/" component = {Landing} />
+        </Switch>
+      </View>
+    </NativeRouter>
   );
 }
 
