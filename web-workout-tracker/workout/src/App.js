5cca5b8ee71887540359f54951625df925c3c8e9
Kristea Gamel <krgamel@gmail.com>
2019-08-20 19:45:30 -0500

    basic Auth 0 implemented
    basic auth 0 implemented with conditional login/logout button in navbar componenet

--- web-workout-tracker/workout/src/App.js
+++ web-workout-tracker/workout/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Landing from './components/Landing'
+import Landing from "./components/Landing";
+
+import NavBar from "./components/Navbar";
+import { useAuth0 } from "./react-auth0-wrapper";
 
 function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
+      <NavBar />
       <Router>
-        <Route exact path = "/" component = {Landing} />
+        <Route exact path="/" component={Landing} />
       </Router>
     </div>
   );
--- web-workout-tracker/workout/src/index.js
+++ web-workout-tracker/workout/src/index.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
+
+import { Auth0Provider } from "./react-auth0-wrapper";
+import config from "./auth_config.json";
+
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
 
 ReactDOM.render(
-    <Router>
+  <Router>
+    <Auth0Provider
+      domain={config.domain}
+      client_id={config.clientId}
+      redirect_uri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
       <Route path="/" component={App} />
-    </Router>,
-    document.getElementById('root'));
+    </Auth0Provider>
+  </Router>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
