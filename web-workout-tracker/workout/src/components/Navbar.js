679afd33d576ff708c3792e82e9a6bda65649182
Kristea Gamel <krgamel@gmail.com>
2019-08-20 19:20:16 -0500

    navbar set

--- web-workout-tracker/workout/src/components/Navbar.js
+++ web-workout-tracker/workout/src/components/Navbar.js
@@ -1,16 +1,20 @@
+// src/components/NavBar.js
+
 import React from "react";
 import { useAuth0 } from "../react-auth0-wrapper";
 
 const NavBar = () => {
-  const { isAuthenticated, loginWIthRedirect, logout } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
   return (
     <div>
       {!isAuthenticated && (
-        <button onClick={() => loginWIthRedirect({})}>Log in</button>
+        <button onClick={() => loginWithRedirect({})}>Log in</button>
       )}
 
       {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
     </div>
   );
 };
+
+export default NavBar;
