33a7f277b39b8ee4897b932b0cbfac0d7b44e51a
Sean Wu <seanwu20@gmail.com>
2019-08-21 14:02:26 -0400

    finished basic form functionality to enter workouts. sorting through some bugs

--- web-workout-tracker/workout/src/components/InputWorkoutForm.js
+++ web-workout-tracker/workout/src/components/InputWorkoutForm.js
@@ -4,11 +4,25 @@ const InputWorkoutForm = (props) => {
 
     const [weightOrCardio, setWeightOrCardio] = useState('');
 
+    const [data, setData] = useState([]);
+
 
     return (
         <div className="input-workout-form">
             {weightOrCardio === '' ? <ChooseForm setWeightOrCardio={setWeightOrCardio}/> :
-                <InputForm weightOrCardio={weightOrCardio}/>}
+                <InputForm weightOrCardio={weightOrCardio}
+                           setWeightOrCardio={setWeightOrCardio}
+                           setData={setData}
+                           data={data}/>}
+
+
+            {data.map(data => {
+                return (
+                    <div>
+                        {console.log(data)}
+                        {data.exerciseName} {data.measurement} {data.unit}
+                    </div>)
+            })}
         </div>
     );
 };
@@ -21,17 +35,55 @@ const ChooseForm = ({setWeightOrCardio}) => {
     };
     return (
         <form>
+            <legend>What kind of workout did you do?</legend>
             <button value='weightlifting' onClick={chooseHandler}>Weightlifting?</button>
             <button value='cardio' onClick={chooseHandler}>Cardio?</button>
         </form>
     )
 };
 
-const InputForm = () => {
+const InputForm = ({weightOrCardio, setWeightOrCardio, setData, data}) => {
+    const backHandler = (event) => {
+        event.preventDefault();
+        setWeightOrCardio('')
+    };
+
+    const [inputData, setInputData] = useState({});
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        setData([...data, inputData])
+    };
+
+    const onChangeHandler = (event) => {
+        setInputData({...inputData, [event.target.name]: event.target.value});
+
+        console.log(inputData)
+    };
+
     return (
-        <form>
-            
-        </form>
+        <>
+            <button onClick={backHandler}>Go back</button>
+            <form onSubmit={submitHandler}>
+                <p>{weightOrCardio}</p>
+                <label>Input your workout info</label>
+                <input type='text'
+                       name='exerciseName'
+                       placeholder='Enter your exercise'
+                       onChange={onChangeHandler}/>
+                <input type='number'
+                       min='1'
+                       max='1000'
+                       name='measurement'
+                       placeholder='Enter measurement'
+                       onChange={onChangeHandler}/>
+                <select name='unit' onChange={onChangeHandler}>
+                    <option value="time">{weightOrCardio === 'weightlifting' ? 'reps' : 'distance'}</option>
+                    <option value="distance">{weightOrCardio === 'weightlifting' ? 'weight' : 'minutes'}</option>
+                </select>
+                <button type='submit'>Submit</button>
+            </form>
+        </>
     )
 
 };
