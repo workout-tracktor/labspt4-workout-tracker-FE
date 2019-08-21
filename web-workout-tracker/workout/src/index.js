33de60bf0f14d89602ffd3c4a9f7459546d4c6bc
Kristea Gamel <krgamel@gmail.com>
2019-08-20 21:13:48 -0500

    deleted extra spacing

--- web-workout-tracker/workout/src/index.js
+++ web-workout-tracker/workout/src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from './serviceWorker';
 
 import { Auth0Provider } from "./react-auth0-wrapper";
 import config from "./auth_config.json";
@@ -21,17 +21,16 @@ const onRedirectCallback = appState => {
 };
 
 ReactDOM.render(
-  <Router>
+    <Router>
     <Auth0Provider
-      domain={config.domain}
-      client_id={config.clientId}
-      redirect_uri={window.location.origin}
-      onRedirectCallback={onRedirectCallback}
-    >
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+>
       <Route path="/" component={App} />
-    </Auth0Provider>
-  </Router>,
-  document.getElementById("root")
-);
+      </Auth0Provider>
+    </Router>,
+    document.getElementById('root'));
 
 serviceWorker.unregister();
