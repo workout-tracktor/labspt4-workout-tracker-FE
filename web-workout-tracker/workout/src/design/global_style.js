3263bf046809afa7d8200fb90bb5534e5663c88c
dustinsnoap <email.com>
2019-08-22 01:40:48 -0400

    updated global-styles

--- web-workout-tracker/workout/src/design/global_style.js
+++ web-workout-tracker/workout/src/design/global_style.js
@@ -4,18 +4,7 @@ import {reset} from 'styled-reset'
 const GlobalStyle = createGlobalStyle`
     ${reset}
     //FONTS
-    @font-face {
-        font-family: premier;
-        src: url('/fonts/premier.otf');
-    }
-    @font-face {
-        font-family: misery;
-        src: url('/fonts/misery.otf');
-    }
     //ANIMATIONS
-    @keyframes blink {
-        50% {opacity: 0.0;}
-    }
     //STYLE
     * {box-sizing: border-box}
     html {
@@ -26,14 +15,6 @@ const GlobalStyle = createGlobalStyle`
     body {
         height: 100%;
         width: 100%;
-    }
-    #liftquest {
-        align-items: flex-start;
-        background-color: #030B0F;
-        color: #ccc;
-        display: flex;
-        height: 100%;
-        justify-content: center;
     }
     a {
         color: inherit;
