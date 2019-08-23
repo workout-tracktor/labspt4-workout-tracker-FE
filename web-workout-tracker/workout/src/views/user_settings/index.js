369c4176e3d1c8bdb862cc9c27b6fd4a7c1a6f29
dustinsnoap <email.com>
2019-08-23 00:12:04 -0400

    restarted view

--- web-workout-tracker/workout/src/views/user_settings/index.js
+++ web-workout-tracker/workout/src/views/user_settings/index.js
@@ -1,38 +1,28 @@
 import React from 'react'
 // import { useAuth0 } from "../react-auth0-wrapper"
 
-// import SECTION from './style'
+import SECTION from './style'
 
 class UserSettings extends React.Component {
     state = {
-        username: '',
-        first_name: '',
-        last_name: '',
-        avatar_src: '',
-        fitness_goals: '',
-        personal_stats: '',
+        body_types: [{name: 'Slenderman', src: '1.png'},
+                        {name: 'Boringman', src: '2.png'},
+                        {name: 'Incredibly Big Man', src: '3.png'},
+                    ]
     }
     componentDidUpdate = () => {}
     update_field = e => this.setState({[e.target.id]: e.target.value})
     render = () =>
-        <section className='user-settings'>
-            <h1>...Loading</h1>
-            <img src='./#' alt='temp'/>
-            <span>Username:</span>
-            <input type='text' id='username' onChange={this.update_field} placeholder='username'/>
-            <span>First Name:</span>
-            <input type='text' id='first-name' onChange={this.update_field} placeholder='first name'/>
-            <span>Last Name:</span>
-            <input type='text' id='last-name' onChange={this.update_field} placeholder='last name'/>
-            <span>Body Type:</span>
-            <span>value</span>
-            <span>Goal Type:</span>
-            <span>Radio/Pulldown/Select Image</span>
-            <span>Joined:</span>
-            <span>Date / Time (uneditable)</span>
-            <span>Stats:</span>
-            <span>...list of personal body and workout stats</span>
-        </section>
+        <SECTION className='user-settings'>
+            <div class='ideal-body-type'>
+                {this.state.body_types.map((type, idx) => (
+                    <div className='type' key={idx}>
+                        <img src={`/img/${type.src}`} alt={type.name} />
+                        <span>{type.name}</span>
+                    </div>
+                ))}
+            </div>
+        </SECTION>
 }
 
 export default UserSettings
--- web-workout-tracker/workout/src/views/user_settings/style.js
+++ web-workout-tracker/workout/src/views/user_settings/style.js
@@ -1,4 +1,16 @@
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
-// export default styled.section`
-// `
+export default styled.section`
+    .ideal-body-type {
+        display: flex;
+        .type {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            span {
+                font-size: 2rem;
+            }
+        }
+    }
+`
