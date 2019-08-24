b39fc86a6e6397d981283b6e0ddb754cf71f6dc9
dustinsnoap <email.com>
2019-08-24 16:06:33 -0400

    added units of measurement

--- web-workout-tracker/workout/src/views/user_settings/index.js
+++ web-workout-tracker/workout/src/views/user_settings/index.js
@@ -14,13 +14,25 @@ class UserSettings extends React.Component {
     update_field = e => this.setState({[e.target.id]: e.target.value})
     render = () =>
         <SECTION className='user-settings'>
-            <div class='ideal-body-type'>
+            <div className='ideal-body-type'>
+                <span className='title'>Ideal Body Type</span>
                 {this.state.body_types.map((type, idx) => (
                     <div className='type' key={idx}>
                         <img src={`/img/${type.src}`} alt={type.name} />
                         <span>{type.name}</span>
                     </div>
                 ))}
+            </div>
+            <div className='units-of-measurment'>
+                <span className='title'>Units of Measurment</span>
+                <div className='unit-types'>
+                    <input type='radio' name='unit-type' value='us' id='us'></input>
+                    <label htmlFor='us'>US Standard</label>
+                </div>
+                <div className='unit-types'>
+                    <input type='radio' name='unit-type' value='metric' id='metric' defaultChecked></input>
+                    <label htmlFor='metric'>Metric</label>
+                </div>
             </div>
         </SECTION>
 }
