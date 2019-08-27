93821980b8f7aceba38d98408136dc98c94843aa
Kristea Gamel <krgamel@gmail.com>
2019-08-27 00:02:34 -0500

    created loading component, added profile and settings to navbar. I think that I may have fixed the loading issue

--- web-workout-tracker/workout/src/components/Navbar.js
+++ web-workout-tracker/workout/src/components/Navbar.js
@@ -1,7 +1,9 @@
 // src/components/NavBar.js
 
 import React from "react";
-import { useAuth0 } from "./auth0-wrapper";
+import { useAuth0 } from "../components/auth0-wrapper";
+
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
@@ -13,6 +15,13 @@ const NavBar = () => {
       )}
 
       {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+
+      {isAuthenticated && (
+        <span>
+          <Link to="/profile">Profile</Link>
+          <Link to="/user-settings">Settings</Link>
+        </span>
+      )}
     </div>
   );
 };
