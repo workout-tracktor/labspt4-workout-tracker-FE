e2e2ed264ccf4abd4c81ffb4c4c0cd5aa4c937a1
Sean Wu <seanwu20@gmail.com>
2019-08-28 20:42:53 -0400

    Pulling from master

--- web-workout-tracker/workout/src/views/App.js
+++ web-workout-tracker/workout/src/views/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 // import { useAuth0 } from "../components/auth0-wrapper";
 
 import NavBar from "../components/Navbar";
@@ -9,33 +9,35 @@ import BodyGoal from "./BodyGoal";
 import UserSettings from "./user_settings";
 // import PrivateRoute from "../components/PrivateRoute";
 import Profile from "./Profile";
-import { useAuth0 } from "../components/auth0-wrapper";
+import {useAuth0} from "../components/auth0-wrapper";
 import Loading from "../components/Loading";
+import InputWorkoutForm from "./InputWorkoutForm";
+
 function App() {
-  const { loading } = useAuth0();
+    const {loading} = useAuth0();
 
-  if (loading) {
-    return <Loading />;
-  }
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <header>
-          <NavBar />
-        </header>
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/user-settings" component={UserSettings} />
+    if (loading) {
+        return <Loading/>;
+    }
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <header>
+                    <NavBar/>
+                </header>
+                <Switch>
+                    <Route exact path="/" component={Landing}/>
+                    <Route exact path="/user-settings" component={UserSettings}/>
 
-          {/* On boarding - choose the body goal */}
-          {/* @TO-DO: Make route private */}
-          <Route exact path="/onboarding/body-goal" component={BodyGoal} />
-          
-          <Route path="/profile" component={Profile} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+                    {/* On boarding - choose the body goal */}
+                    {/* @TO-DO: Make route private */}
+                    <Route exact path="/onboarding/body-goal" component={BodyGoal}/>
+
+                    <Route path="/profile" component={Profile}/>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
