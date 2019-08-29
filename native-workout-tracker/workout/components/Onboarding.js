88823214f65ed5eba7f7aa6f3bac183c6f7e935a
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-08-28 22:13:26 -0700

    flexboxed components

--- native-workout-tracker/workout/components/Onboarding.js
+++ native-workout-tracker/workout/components/Onboarding.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {
+    View, 
+    Text, 
+    StyleSheet, 
+    Button,
+    Dimensions,
+} from 'react-native';
 import TypeCard from './TypeCard';
 import data from './DummyData'
-
 
 class Onboarding extends React.Component {
     //React Navigation comes with a default navbar, which can be styled 
@@ -32,24 +37,28 @@ class Onboarding extends React.Component {
     render() {
         return (
             <View style = {styles.container}>
-                <Text> Goal: Select a body type</Text>
-                {this.generateCards()}
-                <View style = {styles.buttonsContainer}>
-                    <Button
-                     onPress={this.onPressLearnMore}
-                     title = "Select Later"
-                     color = "black"
-                     style = {styles.button}
-                    />
-                    <Button
-                     onPress={this.onPressLearnMore}
-                     title = "SELECT"
-                     color = "black"
-                     style = {styles.button}
-                    />
-                    
+                <View style = {styles.titleContainer}>
+                    <Text style = {styles.title}> Goal: Select a body type</Text>
                 </View>
-
+                <View style = {styles.cards}>
+                    {this.generateCards()}
+                </View>
+                <View style = {styles.buttonsContainer}>
+                    <View style = {styles.buttons}>
+                        <Button
+                        onPress={this.onPressLearnMore}
+                        title = "Select Later"
+                        color = "black"
+                        style = {styles.button}
+                        />
+                        <Button
+                        onPress={this.onPressLearnMore}
+                        title = "SELECT"
+                        color = "black"
+                        style = {styles.button}
+                        />
+                    </View>
+                </View>
             </View>
         )
     }
@@ -58,13 +67,31 @@ class Onboarding extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: 'red',
+        justifyContent: "center",
+        flex: 1
+    },
+    titleContainer: {
         flex: 1,
-        justifyContent: "center"
+    },
+    title: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '10%',
+        padding: 20
     },
     buttonsContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-around'
+        flex: 1.2
     },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        margin: 25,
+    },
+    button: {
+        alignSelf: 'center',
+        width: 500
+    }
 })
 
 export default Onboarding;
