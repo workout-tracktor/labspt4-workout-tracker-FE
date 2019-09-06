0f6a26486d16187d225375cd5353bf8cc7b4df29
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-09-05 20:05:02 -0700

    added navbar with flexbox

--- web-workout-tracker/workout/src/components/Navbar.js
+++ web-workout-tracker/workout/src/components/Navbar.js
@@ -2,8 +2,13 @@
 
 import React from "react";
 import {useAuth0} from "../components/auth0-wrapper";
-
+import styled from "styled-components";
 import {Link} from "react-router-dom";
+import liftQuestTitle from '../assets/images/LiftQuestTitle.png'
+import GearIcon from '../assets/icons/Gear.svg'
+import PersonIcon from '../assets/icons/Person.svg'
+
+import Button from '../components/Button'
 
 const NavBar = () => {
     const {isAuthenticated, loginWithRedirect, logout} = useAuth0();
@@ -11,21 +16,91 @@ const NavBar = () => {
     return (
         <div>
             {!isAuthenticated && (
-                <button onClick={() => loginWithRedirect({})}>Log in</button>
+                <Container >
+                    <img src = {liftQuestTitle}/>
+                    <Button 
+                        onClick={() => loginWithRedirect({})} 
+                        color = {'white'}
+                        background = {' linear-gradient(#2FDDE4, #2367FF)'}
+                        text = {"LOGIN"}
+                        />
+                </Container>
             )}
 
-            {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
-
             {isAuthenticated && (
-                <span>
-                    <Link to="/profile">Profile</Link>
-                    <Link to="/user-settings">Settings</Link>
-                    <Link to='/input-workout'>Input Workout</Link>
-
-        </span>
+                <Nav>
+                        <Link to="/"> <img src = {liftQuestTitle}/> </Link>
+                    <RightNav> 
+                        <Link to="/user-settings" >
+                        <NavText> 
+                            <Gear src = {GearIcon} alt= 'gear'/>
+                            SETTINGS
+                        </NavText>
+                        </Link>
+                        {isAuthenticated && 
+                            <LogoutContainer>
+                                <Person src = {PersonIcon} alt= 'person'/>
+                                <NavText 
+                                    onClick={() => logout()}
+                                >
+                                    LOGOUT
+                                </NavText>
+                            </LogoutContainer>
+                        }
+                    </RightNav>
+                </Nav>
             )}
         </div>
     );
 };
+
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: space-around;
+    margin: 0 auto;
+    width: 294px;
+`;
+
+const Nav = styled.span`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+const RightNav = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+`
+const Gear = styled.img`
+    margin: 5px;
+` 
+const Person = styled.img`
+    margin: 5px;
+` 
+const NavText = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-family: Roboto Condensed;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 18px;
+`
+
+const LogoutContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin-left: 8px;
+    &:hover{
+        cursor: pointer;
+    }
+` 
 
 export default NavBar;
--- web-workout-tracker/workout/src/views/Landing.js
+++ web-workout-tracker/workout/src/views/Landing.js
@@ -20,8 +20,7 @@ class Landing extends React.Component {
     render(){
         return (
             <>
-            {this.state.api}
-            <Profile />
+                <Profile />
             </>
         )
     }
