cbdb6b560d9a2cbe407c91b31b72d90b89abbe24
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-09-16 19:04:47 -0700

    finished workout component

--- web-workout-tracker/workout/src/views/Landing.js
+++ web-workout-tracker/workout/src/views/Landing.js
@@ -2,12 +2,17 @@ import React from "react";
 import axios from "axios";
 import styled from "styled-components";
 import Calendar from "react-calendar";
-
 import Workouts from './Workouts'
+import NoWorkouts from './NoWorkouts'
 
 class Landing extends React.Component {
   state = {
-    date: new Date()
+    date: new Date(),
+    //when date is clicked on, 
+    workouts: false,
+    //selectedDate is the date that is selected on the calendar
+    //which will then be sent to back end to check to see if there was workout data
+    selectedDate: null
   };
 
   componentDidMount() {
@@ -27,7 +32,12 @@ class Landing extends React.Component {
     return (
       <Container>
           <Calendar onChange={this.onChange} value={this.state.date} />
-          <Workouts />
+          {this.state.Workouts? 
+            <Workouts 
+              //Once connected to backend, if there is workout data on the selected date, passes it as props.
+            />
+            :   <NoWorkouts />         
+          }
       </Container>
     );
   }
