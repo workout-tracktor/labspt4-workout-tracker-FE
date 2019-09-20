4c8b5563d90d7cadd4152f50d72fd741aa8b1d40
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-09-19 19:50:44 -0700

    More tests

--- web-workout-tracker/workout/src/__tests__/BodyGoal.test.js
+++ web-workout-tracker/workout/src/__tests__/BodyGoal.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
 
 import { BodyGoal } from '../views/BodyGoal.js';
@@ -19,4 +19,19 @@ it('renders 8 body goals - 4 for each view', () => {
     const { queryAllByTestId } = render(<BodyGoal />);
     const goalsButtons = queryAllByTestId("body-goal");
     expect(goalsButtons.length).toBe(8); // 4 for desktop and 4 for mobile view
+});
+
+it('renders 1 option button', () => {
+    const { queryAllByTestId } = render(<BodyGoal />);
+    const optionsButtons = queryAllByTestId("option-button");
+    expect(optionsButtons.length).toBe(1); // "Select" button is not rendering from the start 
+});
+
+it('renders 2 option buttons when user choose body goal option', () => {
+    const { queryAllByTestId } = render(<BodyGoal />);
+    const goalButton = queryAllByTestId("body-goal")[0];
+    fireEvent.click(goalButton);
+
+    const optionsButtons = queryAllByTestId("option-button");
+    expect(optionsButtons.length).toBe(2);
 });
