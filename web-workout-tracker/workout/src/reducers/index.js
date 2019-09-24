7c54dc92d01619d779e6ecd142db12df0c76e0f7
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-09-23 19:26:48 -0700

    Added actions and changed reducer to store user object in Redux store after register/login

--- web-workout-tracker/workout/src/reducers/index.js
+++ web-workout-tracker/workout/src/reducers/index.js
@@ -1,18 +1,65 @@
 import { 
     /* action imports here */
+    REGISTER_START,
+    REGISTER_SUCCESS,
+    REGISTER_FAILURE,
+    LOGIN_START,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE,
     ADD_GOAL_START,
     ADD_GOAL_SUCCESS,
     ADD_GOAL_FAILURE
 } from '../actions';
 
 const initialState = {
+    user: [],
     goal: "",
     savingGoal: false,
-    error: ""
+    error: "",
+    isLoggingIn: false,
+    isSigningIn: false
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case REGISTER_START: {
+            return {
+                ...state,
+                isSigningIn: true
+            };
+        }
+        case REGISTER_SUCCESS: {
+            return {
+                ...state,
+                isSigningIn: false,
+                user: action.payload
+            };
+        }
+        case REGISTER_FAILURE: {
+            return {
+                ...state,
+                isSigningIn: false
+            };
+        }
+        case LOGIN_START: {
+            return {
+                ...state,
+                isLoggingIn: true
+            };
+        }
+        case LOGIN_SUCCESS: {
+            return {
+                ...state,
+                isLoggingIn: false,
+                credentials: action.payload
+            };
+        }
+        case LOGIN_FAILURE: {
+            return {
+                ...state,
+                isLoggingIn: false
+            };
+        }
         case ADD_GOAL_START:
             return {
                 ...state,
