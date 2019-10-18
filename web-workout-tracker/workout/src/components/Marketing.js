fafbe9400deed8a745c42a5d4b97cc303691647b
Tevvin <Tevvin7395@icloud.com>
2019-10-17 23:19:45 -0400

    error fixed

--- web-workout-tracker/workout/src/components/Marketing.js
+++ web-workout-tracker/workout/src/components/Marketing.js
@@ -1,11 +1,6 @@
 import React from "react";
 import { useAuth0 } from "../components/auth0-wrapper";
-<<<<<<< HEAD
-import { withRouter } from 'react-router'
-import { Link } from 'react-router-dom';
-=======
-import { withRouter } from "react-router";
->>>>>>> f7bc3e64ac2104081f997fc0118591bd1096059f
+import { connect } from "react-redux";
 // Image imports
 import phoneGuy from "../assets/images/guyWithPhone.jpg";
 import iPhone from "../assets/images/applePhone.png";
@@ -22,27 +17,13 @@ const Marketing = props => {
   React.useEffect(() => {
     //checks to see if user is logged in by checking cookies, and routes them
     //to the dashboard if they have not logged out.
-<<<<<<< HEAD
-    if (document.cookie.indexOf('auth0.is.authenticated') === -1) {
-      return null
-    } else {
-      props.history.push("/validate-user")
-=======
     if (document.cookie.indexOf("auth0.is.authenticated") === -1) {
       return null;
     } else {
       props.history.push("/validate-user");
->>>>>>> f7bc3e64ac2104081f997fc0118591bd1096059f
     }
   });
 
-<<<<<<< HEAD
-  })
-
-
-
-=======
->>>>>>> f7bc3e64ac2104081f997fc0118591bd1096059f
   return (
     <div>
       <meta charSet="utf-8" />
@@ -74,15 +55,9 @@ const Marketing = props => {
           />
           <h1 className="top-cta">
             YOU DO THE WORK
-<<<<<<< HEAD
-              <br />
-            WE'LL DO THE COUNTING
-            </h1>
-=======
             <br />
             WE'LL DO THE COUNTING
           </h1>
->>>>>>> f7bc3e64ac2104081f997fc0118591bd1096059f
           <div className="nav">
             {/*
           line 31
@@ -97,28 +72,6 @@ const Marketing = props => {
         <div className="upper-mid-market">
           <p>
             At LiftQuest we provide a simple, intuitive system that allows you
-<<<<<<< HEAD
-            to stop worrying about keeping track of your workouts. With a
-            platform that is constantly growing and improving right at your
-            fingertips, we empower our users to create, track, and store their
-            daily exercise! You handle the lifting and you leave the counting
-            to us.
-            </p>
-          <img
-            className="onboarding-mock"
-            src={iPhone}
-            alt="iPhone witha app"
-          />
-          <img
-            className="tablet-mock"
-            src={tabletMockup}
-            alt="A tablet and a phone with LiftQuest"
-          />
-          <div className="boxes">
-            <p className="box">
-              Designed from the ground up to make tracking exercises easy.
-              </p>
-=======
             to keep track of your workouts, on your mobile device. With a
             platform that is constantly growing and improving right at your
             fingertips, we empower our users to create, track, and store their
@@ -138,7 +91,6 @@ const Marketing = props => {
             <p className="box">
               Designed from the ground up to make tracking exercises easy.
             </p>
->>>>>>> f7bc3e64ac2104081f997fc0118591bd1096059f
             <p className="box">Take it with you everywhere you go!</p>
           </div>
         </div>
@@ -150,15 +102,7 @@ const Marketing = props => {
             src={featureGraphic}
             alt="A list of Features"
           />
-<<<<<<< HEAD
-          <img
-            className="trips"
-            src={tripleThreat}
-            alt="Mock up screens"
-          />
-=======
           <img className="trips" src={tripleThreat} alt="Mock up screens" />
->>>>>>> f7bc3e64ac2104081f997fc0118591bd1096059f
         </div>
         <div className="footer">
           <footer>
@@ -166,18 +110,6 @@ const Marketing = props => {
         <img class="google-download" src="/resources/img/google-play-badge.png" alt="Download on GooglePlay" />
         <img class="apple-download" src="/resources/img/app-store-badge.svg" alt="Download on App Store" />
         */}
-<<<<<<< HEAD
-            <h1>Get Big!</h1>
-            <a href="https://dev-zjd-v4vy.auth0.com/login?state=g6Fo2SBoVUp1RGp4cEtBMHJUX1BRYWdVbGZMeTVXVFUxMTJ0UaN0aWTZIFFENGNPT3BjWHE1dlBRSTdGeEZ1Z0w2WTZWTERtREdoo2NpZNkgWEQ2b3BXS1UwV2dXSFhKVndwMmxrWnVTdWpnVnAyaXg&client=XD6opWKU0WgWHXJVwp2lkZuSujgVp2ix&protocol=oauth2&redirect_uri=http%3A%2F%2Flocalhost%3A3000&scope=openid%20profile%20email&response_type=code&response_mode=query&nonce=HnG1hDZr6Rzh0FSLzNChi4fCYtU.wAED0KhsSV4Fe-c&code_challenge=iT7_RG510Y2mmd2xE-dW4YMDAqlqcKsrnvNi8yDL2pI&code_challenge_method=S256&auth0Client=eyJuYW1lIjoiYXV0aDAtc3BhLWpzIiwidmVyc2lvbiI6IjEuMS4xIn0%3D">Login</a>
-            <br />
-            <br />
-            <br />
-            <Link to={"/team"}>Team</Link>
-          </footer>
-        </div>
-        <div className="copyright">
-          <p>© LiftQuest 2019</p>
-=======
             <h1>Fitter Faster Further</h1>
             {/* <a href="https://peaceful-joliot-329a9c.netlify.com/">Login</a> */}
             {/* <p>
@@ -188,16 +120,11 @@ const Marketing = props => {
         </div>
         <div className="copyright">
           <p>© 2019 LiftQuest</p>
->>>>>>> f7bc3e64ac2104081f997fc0118591bd1096059f
         </div>
       </div>
       {/* Google Analytics: change UA-XXXXX-Y to be your site's ID. */}
     </div>
   );
-<<<<<<< HEAD
-}
-=======
 };
->>>>>>> f7bc3e64ac2104081f997fc0118591bd1096059f
 
-export default withRouter(Marketing);
+export default connect()(Marketing);
