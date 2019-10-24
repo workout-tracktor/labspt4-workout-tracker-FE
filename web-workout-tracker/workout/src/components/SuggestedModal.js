a0324b030714dd95db4d4a20e79fd9b9403c3357
Mychal <hallmychal@gmail.com>
2019-10-24 01:01:09 -0500

    'CtrlZ'

--- web-workout-tracker/workout/src/components/SuggestedModal.js
+++ web-workout-tracker/workout/src/components/SuggestedModal.js
@@ -7,55 +7,23 @@ import axios from "axios";
 // query
 
 export default class SuggestedModal extends React.Component {
-  constructor(props){
-    super(props)
-    this.state={
-      query: ''
-    }
-  }
-
-  // static getDerivedStateFromProps(props, state) {
-  //   if (props.query !== state.query) {
-  //     return {query: props.query}
-  //   }
-
-  //   // Return null if the state hasn't changed
-  //   return null;
-  // }
-
-  // componentDidUpdate(prevProps) {
-  //   // Typical usage (don't forget to compare props):
-  //   if (this.props.query !== prevProps.query) {
-  //     getRequest()
-  //   }
-  // }
-
-  componentDidUpdate(prevProps){
-    if(prevProps.query !== this.props.query){
-        this.setState({          
-          query: this.props.query
-        });
-    }
-}
 
   getRequest() {
     axios
       .get(
-        `https://wger.de/api/v2/exercise/?language=2&exercisecategory=${this.state.query}&status=2&limit=5`
+        `https://wger.de/api/v2/exercise/?language=2&exercisecategory=${this.props.query}&status=2&limit=5`
       )
       .then(res => {
         console.log(res.data.results);
       });
   }
 
-  render() 
-  {
-    console.log(this.state.query);
-    this.getRequest()
+  render() {
+    console.log(this.props.query);
     if (!this.props.show) {
       return null;
     }
-    return <Modal></Modal>;
+    return <Modal>{this.getRequest()}</Modal>;
   }
 }
 
