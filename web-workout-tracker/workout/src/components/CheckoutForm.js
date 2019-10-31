ced2029ed9bc3530791ba800833c53b8d6ad287d
Kristea Gamel <krgamel@gmail.com>
2019-10-30 19:37:48 -0500

    adding in stripe checkout

---
+++ web-workout-tracker/workout/src/components/CheckoutForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+export default class CheckoutForm extends React.Component {
+  onToken = token => {
+    fetch("/save-stripe-token", {
+      method: "POST",
+      body: JSON.stringify(token)
+    }).then(response => {
+      response.json().then(data => {
+        alert(`We are in business, ${data.email}`);
+      });
+    });
+  };
+
+  render() {
+    return (
+      <StripeCheckout
+        name="LiftQuest" // the pop-in header title
+        amount="300"
+        label="Additional Features" // text inside the Stripe button
+        panelLabel="Pay with 💳"
+        description="Special Features"
+        image="https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/horizontal.svg"
+        locale="auto"
+        name="LiftQuest"
+        stripeKey="pk_test_1MMw6F4AwE6N02FyRrmMDgUw00jBG44S6s"
+        token={this.onToken}
+        zipCode
+      />
+    );
+  }
+}
--- web-workout-tracker/workout/src/views/landing/Landing.js
+++ web-workout-tracker/workout/src/views/landing/Landing.js
@@ -5,13 +5,14 @@ import Calendar from "react-calendar";
 import "../Calendar.css";
 import Workouts from "./Workouts";
 import { connect } from "react-redux";
+import CheckoutForm from "../../components/CheckoutForm";
 
 class Landing extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       date: new Date(),
-      //when date is clicked on, 
+      //when date is clicked on,
       workouts: true,
       //selectedDate is the date that is selected on the calendar
       //which will then be sent to back end to check to see if there was workout data
@@ -22,31 +23,35 @@ class Landing extends React.Component {
   }
   componentDidMount() {
     //checks to see if user is logged in by checking cookie to render components
-    if (document.cookie.indexOf('auth0.is.authenticated') !== -1) {
-      this.setState({ isLoggedin: !this.state.isLoggedin })
-    } else { this.setState({ isLoggedin: false }) }
+    if (document.cookie.indexOf("auth0.is.authenticated") !== -1) {
+      this.setState({ isLoggedin: !this.state.isLoggedin });
+    } else {
+      this.setState({ isLoggedin: false });
+    }
     axios
       .get("https://workouttrackerprod.herokuapp.com/")
       .then(res => {
-
         this.setState(res.data);
       })
       .catch(err => {
         console.log(err);
       });
-
   }
   onChange = date => this.setState({ date });
 
   render() {
     return (
       <Container>
-        {this.state.isLoggedin ?
+        {this.state.isLoggedin ? (
           <>
             <Calendar onChange={this.onChange} value={this.state.date} />
+
             <Workouts />
           </>
-          : null}
+        ) : null}
+        <div>
+          <CheckoutForm />
+        </div>
       </Container>
     );
   }
@@ -67,4 +72,7 @@ const Container = styled.div`
     align-items: center;
   }
 `;
-export default connect(mapStateToProps, {})(Landing);
+export default connect(
+  mapStateToProps,
+  {}
+)(Landing);
