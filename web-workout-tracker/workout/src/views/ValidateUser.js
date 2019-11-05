0ac310a0bb43d8400a726cc555f08fab10f7dfb7
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-11-05 00:28:31 -0800

    fixed validate user on staging

--- web-workout-tracker/workout/src/views/ValidateUser.js
+++ web-workout-tracker/workout/src/views/ValidateUser.js
@@ -1,63 +1,65 @@
 // src/components/Profile.js
 
 import React, {useEffect, useState} from "react";
-import {useAuth0} from "../components/auth0-wrapper";
+import { useAuth0 } from "../components/auth0-wrapper";
 import axios from "axios";
 import {getBodyGoal} from '../actions'
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
 // import {post}
 const ValidateUser = (props) => {
-    const {user} = useAuth0();
+  const { user } = useAuth0();
+  
+  useEffect(() => {
+    // Update the document title using the browser API
 
-    useEffect(() => {
-        // Update the document title using the browser API
+    if(user === undefined){
+       props.history.push("/Landing")    
+    } else {
+      const userData = {
+        first_name: user.given_name,
+        last_name: user.family_name,
+        username: user.nickname,
+        email: user.email,
+        avatar: user.picture,
+        password: user.sub,
+        units_system: 'metric'
+      }
 
-        if (user === undefined) {
-            props.history.push("/Landing")
-        } else {
-            const userData = {
-                first_name: user.given_name,
-                last_name: user.family_name,
-                username: user.nickname,
-                email: user.email,
-                avatar: user.picture,
-                password: user.sub,
-                units_system: 'metric'
-            }
+      axios
+      //sends GET request to send user backend data to Redux store
+      .get(`https://workouttrackerstaging-2.herokuapp.com/api/user?username=${user.nickname}`)
+      .then(res => {
+        //If successful GET by username, sends to dashboard, and sets the navbar to render
+        //correct buttons
+        props.newUserToggler(false)
+        props.getBodyGoal(res.data)
 
-            axios
-            //sends GET request to send user backend data to Redux store
-                .get(`https://workouttrackerstaging-2.herokuapp.com/api/exercise?username=${user.nickname}`)
-                .then(res => {
-                    //If successful GET by username, sends to dashboard, and sets the navbar to render
-                    //correct buttons
-                    props.newUserToggler(false)
-                    props.getBodyGoal(res.data)
-                    localStorage.setItem("user_id", res.data.user_id)
-                    props.history.push("/Landing")
-                })
-                .catch(err => {
-                    //If failed 404 Not Found nickname, sends to onboarding
-                    axios.post('https://workouttrackerstaging-2.herokuapp.com/api/user', userData)
-                        .then(res => {
-                            props.getBodyGoal(res.data)
-                            localStorage.setItem("user_id", res.data.user_id)
-                            props.history.push("/onboarding/body-goal")
-                            //sets the nav bar to hide certain buttons if new user
-                            props.newUserToggler(true)
-                        })
-                })
-        }
-    });
-    return (
-        <>
-        </>
-    );
+        localStorage.setItem("user_id", res.data.user_id )
+        props.history.push("/Landing")    
+      })
+      .catch(err => {
+        //If failed 404 Not Found nickname, sends to onboarding
+        axios.post(`https://workouttrackerstaging-2.herokuapp.com/api/user`, userData)
+        .then(res => {
+          props.getBodyGoal(res.data)
+          localStorage.setItem("user_id", res.data.user_id )
+          props.history.push("/onboarding/body-goal" )
+          //sets the nav bar to hide certain buttons if new user
+          props.newUserToggler(true)
+        })
+      })
+      }
+  });
+  return (
+    <>
+    </>
+  );
 };
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+});
 
 export default connect(
-    mapStateToProps,
-    {getBodyGoal}
-)(ValidateUser)
+  mapStateToProps, 
+  {getBodyGoal}
+  )(ValidateUser)
