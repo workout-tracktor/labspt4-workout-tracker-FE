a6ec5f900e750362fae0fd5cda8ab35f770f7978
miguel diaz <diaz4674@gmail.com>
2020-03-26 21:21:08 -0700

    updated onboarding images

--- web-workout-tracker/workout/src/assets/data/ButtonWithBackgroundData.js
+++ web-workout-tracker/workout/src/assets/data/ButtonWithBackgroundData.js
@@ -1,31 +1,43 @@
+// The links below are commented out because url image links currently down
+
 module.exports =   {
     data : [
         {
             // Slim down
             id: 1,
-            urlMobile: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/slim-down-mobile.png",
-            url: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/slim-down-tablet-desktop.png",
+            urlMobile: "https://images.pexels.com/photos/416778/pexels-photo-416778.jpeg?cs=srgb&dl=active-adult-athlete-body-416778.jpg&fm=jpg",
+            url: "https://images.pexels.com/photos/416778/pexels-photo-416778.jpeg?cs=srgb&dl=active-adult-athlete-body-416778.jpg&fm=jpg",
+            // urlMobile: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/slim-down-mobile.png",
+            // url: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/slim-down-tablet-desktop.png",
             text: "Slim down"
         },
         // Get big
         {
             id: 2,
-            urlMobile: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/get-big-mobile.png",
-            url: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/get-big-tablet-desktop.png",
+            urlMobile: "https://images.pexels.com/photos/38630/bodybuilder-weight-training-stress-38630.jpeg?cs=srgb&dl=blue-and-red-superman-print-tank-top-shirt-38630.jpg&fm=jpg",
+            url: "https://images.pexels.com/photos/38630/bodybuilder-weight-training-stress-38630.jpeg?cs=srgb&dl=blue-and-red-superman-print-tank-top-shirt-38630.jpg&fm=jpg",
+            // Links below are commented out because links currently down
+            // urlMobile: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/get-big-mobile.png",
+            // url: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/get-big-tablet-desktop.png",
             text: "Get big"
         },
         // Maintain
         {
             id: 3,
-            urlMobile: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/maintain-mobile.png",
-            url: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/maintain-tablet-desktop.png",
+            urlMobile: "https://images.pexels.com/photos/1300526/pexels-photo-1300526.jpeg?cs=srgb&dl=woman-exercising-bear-body-of-water-1300526.jpg&fm=jpg",
+            url: "https://images.pexels.com/photos/1300526/pexels-photo-1300526.jpeg?cs=srgb&dl=woman-exercising-bear-body-of-water-1300526.jpg&fm=jpg",
+            // urlMobile: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/maintain-mobile.png",
+            // url: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/maintain-tablet-desktop.png",
             text: "Maintain"
         },
         // Athletic
         {
             id: 4,
-            urlMobile: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/athletic--mobile.png",
-            url: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/athletic-tablet-desktop.png",
+        
+            urlMobile: "https://images.pexels.com/photos/936094/pexels-photo-936094.jpeg?cs=srgb&dl=man-wearing-white-sweater-and-black-shorts-about-to-run-936094.jpg&fm=jpg",
+            url: "https://images.pexels.com/photos/3253501/pexels-photo-3253501.jpeg?cs=srgb&dl=man-holding-brown-rope-3253501.jpg&fm=jpg",
+            // urlMobile: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/athletic--mobile.png",
+            // url: "https://lift-quest-logo-staging.s3.us-east-2.amazonaws.com/athletic-tablet-desktop.png",
             text: "Athletic"
         }
     ]
